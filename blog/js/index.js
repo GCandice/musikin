9b6203e488a65fe767127a6931479eb0d12ea8d8
cloelh <cloelh@outlook.fr>
2019-01-07 16:01:35 +0100

    Version initiale

---
+++ blog/js/index.js
@@ -0,0 +1,29 @@
+//art13
+$('.button3').click(function(){
+  $('.pop-up3').addClass('open');
+});
+
+$('.pop-up3 .close').click(function(){
+  $('.pop-up3').removeClass('open');
+});
+
+
+
+//art2
+$('.button2').click(function(){
+  $('.pop-up2').addClass('open');
+});
+
+$('.pop-up2 .close').click(function(){
+  $('.pop-up2').removeClass('open');
+});
+
+
+//art1
+$('.button1').click(function(){
+  $('.pop-up1').addClass('open');
+});
+
+$('.pop-up1 .close').click(function(){
+  $('.pop-up1').removeClass('open');
+});
